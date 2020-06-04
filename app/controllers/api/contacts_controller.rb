acf5b84b63b4ff4587cbf6a2b7b03cce60ddb915
Colin <colintklein@gmail.com>
2020-06-04 12:50:23 -0500

    Adds lat and lon to views, create method, update method in controller. Adds geocoder gem to allow user to input address and have it converted into coords in contact.

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -8,13 +8,16 @@ class Api::ContactsController < ApplicationController
     render "all_contacts.json.jb"
   end
   def create
+    coords = Geocoder.coordinates(params[:address])
     @contact = Contact.create(
       first_name: params[:first_name],
       middle_name: params[:middle_name],
       last_name: params[:last_name],
       email: params[:email],
       phone_number: params[:phone_number],
-      bio: params[:bio]
+      bio: params[:bio],
+      lat: coords[0],
+      lon: coords[1]
     )
     render 'one_contact.json.jb'
   end
@@ -27,6 +30,8 @@ class Api::ContactsController < ApplicationController
     @contact.email = params[:email] || @contact.email
     @contact.phone_number = params[:phone_number] || @contact.phone_number
     @contact.bio = params[:bio] || @contact.bio
+    @contact.lat = params[:lat] || @contact.lat
+    @contact.lon = params[:lon] || @contact.lon
     @contact.save
     render 'one_contact.json.jb'
   end
